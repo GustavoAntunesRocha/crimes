44bc5de6329796ffb74cc9b2a06374b5f0b51e12
Unknown <gustavorocha@inf.ufg.br>
2017-11-23 21:04:39 -0200

    Criado entidade crime

---
+++ src/main/java/jpa/Crime.java
@@ -0,0 +1,53 @@
+package jpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Crime {
+	@Id
+	private int id;
+	
+	private String tipo;
+	
+	private String arma;
+	
+	private String descricao;
+	
+	private String imagePath;
+	
+	public void setID(int id) {
+		this.id = id;
+	}
+	public int getID() {
+		return this.id;
+	}
+	
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getTipo() {
+		return this.tipo;
+	}
+	
+	public void setArma(String arma) {
+		this.arma = arma;
+	}
+	public String getArma() {
+		return this.arma;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getDescricao() {
+		return this.descricao;
+	}
+	
+	public void setImagePath(String path) {
+		this.imagePath = path;
+	}
+	public String getImagePath() {
+		return this.imagePath;
+	}
+}
